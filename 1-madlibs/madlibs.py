5b5968e5640ab384f072d1066b794d70942705fc
sofiamejiamuro <sofiamejiamuro@gmail.com>
2024-01-28 20:20:58 -0600

    project 1 - MADLIBS

---
+++ 1-madlibs/madlibs.py
@@ -0,0 +1,18 @@
+# string conncatenation
+
+# youtuber = "me"
+
+# few ways to do so
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb_1 = input("Verb: ")
+verb_2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time beacuse \
+I love to {verb_1}. Stay hydrated and {verb_2} like you are {famous_person}"
+
+print(madlib)
